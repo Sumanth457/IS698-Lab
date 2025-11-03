19398e164fdc14a3c8c6f78eae03af6e31bd1704
Sumanth457 <saisumk1@umbc.edu>
2025-11-03 15:14:26 -0500

    Add files via upload

---
+++ Lab7/terraform-multiple-instances/foreach_main.tf
@@ -0,0 +1,26 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+variable "instances" {
+  type = map
+  default = {
+    "web1" = "t3.micro"
+    "web2" = "t3.micro"
+    "web3" = "t3.small"
+  }
+}
+
+resource "aws_instance" "web" {
+  for_each = var.instances
+
+  ami           = "ami-0bdd88bd06d16ba03"
+  instance_type = each.value
+
+  tags = {
+    Name = "${each.key}"
+  }
+}
+
